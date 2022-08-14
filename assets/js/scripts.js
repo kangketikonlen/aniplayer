342722707f2750a25ecd2ffe7b5b70570e71e09c
Gilang Pratama <pratamapriadi96@gmail.com>
2022-08-15 02:45:01 +0700

    Update scripts

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,23 +1,33 @@
-const player = videojs('video');
+const player = videojs('video', {
+	fill: true,
+	aspectRatio: '16:9',
+	fullscreen: {
+		enterOnRotate: true,
+		exitOnRotate: true,
+		alwaysInLandscapeMode: true,
+		iOS: true
+	}
+});
+const homeBtnEl = document.createElement('button');
+const nextBtnEl = document.createElement('button');
+const prevBtnEl = document.createElement('button');
+
 let videos = [];
+
 var url = window.location.href;
 var title = getUrlVars()["t"];
 
-for (var i = 0; i < 25; i++) {
-	episode = pad(i + 1, 2);
-	videos[i] = {
-		sources: [
-			{
-				src: `server/videos/${title}-${episode}.mp4`,
-				type: 'video/mp4'
-			}
-		],
-		poster: 'http://media.w3.org/2010/05/sintel/poster.png'
-	};
-}
+homeBtnEl.classList.add("vjs-button");
+nextBtnEl.classList.add("vjs-button");
+prevBtnEl.classList.add("vjs-button");
 
-player.playlist(videos);
-player.playlist.autoadvance(0);
+homeBtnEl.innerHTML = "Home";
+nextBtnEl.innerHTML = "Next";
+prevBtnEl.innerHTML = "Prev";
+
+homeBtnEl.setAttribute("onclick", "homeBtn()");
+nextBtnEl.setAttribute("onclick", "nextBtn()");
+prevBtnEl.setAttribute("onclick", "prevBtn()");
 
 function pad(str, max) {
 	str = str.toString();
@@ -33,4 +43,47 @@ function getUrlVars() {
 		vars[hash[0]] = hash[1];
 	}
 	return vars;
+}
+
+if (title) {
+	for (var i = 0; i < 25; i++) {
+		episode = pad(i + 1, 2);
+		videos[i] = {
+			sources: [
+				{
+					src: `server/videos/${title}/${title}-${episode}.mp4`,
+					type: 'video/mp4'
+				}
+			],
+			poster: '/aniplayer/assets/images/01-amelia-poster.jpeg'
+		};
+	}
+	player.addClass('vjs-tailwind');
+	player.landscapeFullscreen();
+	player.playlist(videos);
+	player.playlist.autoadvance(0);
+	player.controlBar.el().insertBefore(homeBtnEl, player.controlBar.el().firstChild.nextSibling)
+	player.controlBar.el().insertBefore(nextBtnEl, player.controlBar.el().firstChild.nextSibling)
+	player.controlBar.el().insertBefore(prevBtnEl, player.controlBar.el().firstChild)
+
+	document.onkeydown = (e) => {
+		e = e || window.event;
+		if (e.keyCode === 37) {
+			player.playlist.previous();
+		} else if (e.keyCode === 39) {
+			player.playlist.next();
+		}
+	}
+
+	function homeBtn() {
+		window.location.href = "index.html";
+	}
+
+	function nextBtn() {
+		player.playlist.next();
+	}
+
+	function prevBtn() {
+		player.playlist.previous();
+	}
 }
